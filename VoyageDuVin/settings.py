6c924191ba7081ef80b63ceea07616b531b3320c
Jay <roebuck.jayc+github@gmail.com>
2023-11-19 18:52:30 -0500

    prepare for prod

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")] if os.environ.get("ALLOWED_HOSTS") is not None else []
 
 
 # Application definition
