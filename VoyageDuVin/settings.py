56084cf565f238bb2cd00955591e575476ab7331
Jay <roebuck.jayc+github@gmail.com>
2023-11-19 18:25:15 -0500

    added safe SECRET_KEY

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4zp3s-y_-2b+ffk*k)nlh#su1lh86tr=4gyn@08q+3%8+4wc#$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
