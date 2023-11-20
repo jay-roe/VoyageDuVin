441e9b2eec7fd7c865f133bba768bdc35e045127
Jay <roebuck.jayc+github@gmail.com>
2023-11-19 19:07:49 -0500

    improve ALLOWED_HOSTS env var

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")] if os.environ.get("ALLOWED_HOSTS") is not None else []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
