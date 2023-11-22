abce8ad521a8f3426395b4863785cb619f6c7de0
Jay <roebuck.jayc+github@gmail.com>
2023-11-22 16:30:09 -0500

    added models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Wine, Session, UserScore, WineScore
+
+admin.site.register(Tag)
+admin.site.register(Wine)
+admin.site.register(Session)
+admin.site.register(UserScore)
+admin.site.register(WineScore)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.FileField(upload_to="tags")
+
+    def __str__(self):
+        # Return a string that represents the instance
+        return self.name
+
+
+class Wine(models.Model):
+    short_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=100)
+    image = models.FileField(upload_to="wines")
+    order = models.IntegerField()  # ex: 3rd wine of the session
+    variety = models.CharField(max_length=100)  # cepage
+    region = models.CharField(max_length=100)  # country, specific region
+    alcohol_content = models.FloatField()  # in %
+    sweetness = models.FloatField()  # in g/L
+    tags = models.ManyToManyField(Tag)  # natural, organic etc.
+
+    def __str__(self):
+        # Return a string that represents the instance
+        return self.short_name
+
+
+class Session(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    wines = models.ManyToManyField(Wine)
+
+    def __str__(self):
+        # Return a string that represents the instance
+        return f"{self.name} - {self.date}"
+
+
+class UserScore(models.Model):  # All scores for a user
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        # Return a string that represents the instance
+        return f"{self.name}'s scores"
+
+
+class WineScore(models.Model):
+    user_score = models.ForeignKey(UserScore, on_delete=models.CASCADE)
+    wine = models.ForeignKey(Wine, on_delete=models.CASCADE)
+    score = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
