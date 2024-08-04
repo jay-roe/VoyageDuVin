6916bbfcb5781416b2983f9257296ef711cbcc08
KELLOGGS522 <laguimauve522@gmail.com>
2024-08-04 14:30:02 -0400

    readd debug env var

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "23232")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
