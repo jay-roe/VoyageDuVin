239bc79f5dec2bb9613903c3e49d9c41e575534b
KELLOGGS522 <laguimauve522@gmail.com>
2024-08-10 23:25:14 -0400

    Fix settings

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -85,14 +85,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } if LOCAL else {
-        {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.getenv("DB_NAME", 'myproject'),
-            'USER': os.getenv("DB_USER", 'myprojectuser'),
-            'PASSWORD': os.getenv("DB_PASSWORD", 'password'),
-            'HOST': os.getenv("DB_HOST", 'localhost'),
-            'PORT': os.getenv("DB_PORT", ""),
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME", 'myproject'),
+        'USER': os.getenv("DB_USER", 'myprojectuser'),
+        'PASSWORD': os.getenv("DB_PASSWORD", 'password'),
+        'HOST': os.getenv("DB_HOST", 'localhost'),
+        'PORT': os.getenv("DB_PORT", ""),
     }
 }
 
