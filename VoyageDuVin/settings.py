7177b00233646cabce8b87e1bb090a36b43f0f36
KELLOGGS522 <laguimauve522@gmail.com>
2024-08-10 23:22:56 -0400

    Adding postgresql db

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -31,6 +31,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+LOCAL = os.getenv("LOCAL", "False") == "True"
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -82,6 +84,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    } if LOCAL else {
+        {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv("DB_NAME", 'myproject'),
+            'USER': os.getenv("DB_USER", 'myprojectuser'),
+            'PASSWORD': os.getenv("DB_PASSWORD", 'password'),
+            'HOST': os.getenv("DB_HOST", 'localhost'),
+            'PORT': os.getenv("DB_PORT", ""),
+        }
     }
 }
 
