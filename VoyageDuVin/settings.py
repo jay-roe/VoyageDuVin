dd0de2c30c6168c02dd0b362afb1d0dd3c7b555b
KELLOGGS522 <laguimauve522@gmail.com>
2024-11-22 17:23:12 -0500

    test fix media url

--- VoyageDuVin/settings.py
+++ VoyageDuVin/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
